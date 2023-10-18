5908adfa08115b3fd3c195732b7c9bbd5901ae8a
xavi <xavilok@gmail.com>
2023-10-18 11:28:02 +0200

    se sube la lista de usuario y get en ListaUsuario

--- src/components/usuario/ListaUsuarios.jsx
+++ src/components/usuario/ListaUsuarios.jsx
@@ -1,9 +1,55 @@
+import axios from 'axios'
+import './Formulario.css'
+import { useEffect, useState } from 'react'
+import { NavLink } from "react-router-dom"
+import { Icon } from '@iconify/react';
 
+const url = "http://localhost:8086/api/v1/oficina"
+const ListaUsuario = () => {
 
-const ListaUsuarios = () => {
-  return (
-    <div>ListaUsuarios</div>
-  )
+    const [characters, setCharacters] = useState([])
+
+    useEffect(() => {
+        getAllcharacters()
+    }, [])
+
+    const getAllcharacters = async () => {
+        const response = await axios.get(url)
+        let data = response.data
+        setCharacters(data)
+    }
+
+    const handleDelete = async (id) => {
+        await axios.delete(`${url}/${id}`);
+        setCharacters((prevCharacter) => prevCharacter.filter((character) => character.id !== id));
+        alert(`ATENCION! ELIMINANDO REPOSITORIO ID#${id}`);
+    };
+
+    getAllcharacters()
+
+    return (
+        <>
+            {
+                characters.map(character => (
+                    <div className='contenedor-api1' key={character.id}>
+                        <div className='contenedor-api2'>
+                            <div className='card-api'>
+                                <h3>{character.Estado}</h3>
+                                <div className='image-contenedor'>
+                                <img src={character.Oficina} alt="" />
+                                </div>
+                                <p>{character.Puesto}</p>
+                                <p>{character.Usuario}</p>
+                                <NavLink to={`/editar/${character.id}`}><button className='boton-edit' ><Icon icon="bxs:edit" color="blue" /></button></NavLink>
+                                <button className="boton-borrar" onClick={() => handleDelete(character.id)}><Icon icon="bi:trash-fill" color="maroon" /></button>
+                            </div>
+                        </div>
+                    </div>
+                ))
+            }
+
+        </>
+    )
 }
 
-export default ListaUsuarios
+export default ListaUsuario
