2185b9e0c36b82146bdbac4a4ce5a046e0793760
AkagamiNoLorea <raiseta92@gmail.com>
2023-10-19 23:59:31 +0200

    cnavi letra

--- src/components/puestos/ElegirOficina.jsx
+++ src/components/puestos/ElegirOficina.jsx
@@ -1,11 +1,43 @@
-
-/*Aquí se va a ver una lista cards (cada card es una oficina),
-solo es necesario poder clicar en la card, para que te llebe a la lista de puestos adecuada*/
-/*llamada y visualización cards oficinas, selección id oficina*/
+import axios from 'axios'
+  import { useEffect, useState } from 'react'
+  import { NavLink } from "react-router-dom"
+  import { Icon } from '@iconify/react';
+  
+  const url = "http://localhost:8086/api/v1/Usuariopuesto"
 const ElegirOficina = () => {
-  return (
-    <div>ElegirOficina</div>
-  )
-}
+  
+      const [oficina, setOficina] = useState([])
+  
+      useEffect(() => {
+          getAlloficinas()
+      }, [])
+  
+      const getAlloficinas = async () => {
+          const response = await axios.get(url)
+          let data = response.data
+          setOficina(data)
+      }
+
+  
+      getAllOficinas()
+  
+      return (
+          <>
+              {
+                  oficina.map(oficina => (
+                      <div className='contenedor-api1' key={oficina.id}>
+                          <div className='contenedor-api2'>
+                              <div className='card-api'>
+                                  <h3>{oficina.NombreOficina}</h3>
+                                  <NavLink to={`/private/editarpuestos/${oficina.id}`}><button className='boton-edit' ><Icon icon="bxs:edit" color="blue" /></button></NavLink>
+                              </div>
+                          </div>
+                      </div>
+                  ))
+              }
+  
+          </>
+      )
+  }
 
 export default ElegirOficina
