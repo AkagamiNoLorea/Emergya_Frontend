b6eee1898827012a55fe29a52684090ff18fe6f4
AkagamiNoLorea <raiseta92@gmail.com>
2023-10-19 12:42:04 +0200

    añadida parte de Xavi y editados formatos

--- src/components/oficinas/CrearOficina.jsx
+++ src/components/oficinas/CrearOficina.jsx
@@ -1,10 +1,60 @@
-import React from 'react'
-/*En esta parte habrá un formulario para crear una oficina, apartados(nombre, numero de puestos,plano
-    de distribucion y dias antelacion), con boton de confirmar y cancelar*//*funcion post en api*/
+import axios from "axios"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import '../estilos/Forms.css'
+
+const url = "http://localhost:8086/api/v1/oficina"
+
 const CrearOficina = () => {
-  return (
-    <div>CrearOficina</div>
-  )
+
+    const [Nombreoficina, setNombreoficina,] = useState('')
+    const [Puesto, setPuesto] = useState('')
+    const [Planodistribucion, setPlanodistribucion] = useState('')
+    const [Diasantelacion, setDiasantelacion] = useState('')
+
+    const navigate = useNavigate()
+
+    const guardar = async (e) => {
+        e.preventDefault()
+        await axios.post(url, { Nombreoficina: Nombreoficina, Puesto: Puesto, Planodistribucion: Planodistribucion, Diasantelacion: Diasantelacion })
+        navigate("/proyectos")
+    }
+
+    return (
+        <>
+            <form onSubmit={guardar}>
+            <div className="applicationForm">
+            <h2>Crear Repositorio</h2>
+          </div>
+            <div className="contenedor-form">
+                    <div className="formTable">
+                            <div>
+                                <label>Titulo:</label>
+                                <input type="text" value={Nombreoficina} onChange={(e) => setNombreoficina(e.target.value)} />
+                            </div>
+                            <div>
+                                <label>Imagen:</label>
+                                <input type="text" value={Puesto} onChange={(e) => setPuesto(e.target.value)} />
+                            </div>
+                            <div>
+                                <label>Git:</label>
+                                <input type="text" value={Planodistribucion} onChange={(e) => setPlanodistribucion(e.target.value)} />
+                            </div>
+                            <div>
+                                <label>Descripción:</label>
+                                <textarea name="" id="" value={Diasantelacion} onChange={(e) => setDiasantelacion(e.target.value)}>
+
+                                </textarea>
+                            </div>
+                            <div className='form2Buttons'>
+              <button className='formButton' type="submit"><i className="fa-regular fa-floppy-disk"></i> Guardar canvios</button>
+              <button className='formButton' type="button"><i className="fa-solid fa-xmark"></i> Cancelar</button>
+            </div>
+                    </div>
+                </div>
+                </form>
+        </>
+    )
 }
 
 export default CrearOficina
--- src/components/usuario/EditarUsuario.jsx
+++ src/components/usuario/EditarUsuario.jsx
@@ -1,8 +1,71 @@
-import React from 'react'
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+import { Icon } from '@iconify/react';
+import fondo from '../assets/img/fondo.jpg'
+
+const url = "http://localhost:8086/api/v1/oficina"
 
 const EditarUsuario = () => {
+
+  const [Estado, setEstado] = useState('')
+  const [Oficina, setOficina] = useState('')
+  const [Puesto, setPuesto] = useState('')
+  const [Usuario, setUsuario] = useState('')
+  const navigate = useNavigate()
+
+  const { id } = useParams()
+
+  const update = async (e) => {
+    e.preventDefault()
+    await axios.put(`${url}/${id}`, { Estado: Estado, Oficina: Oficina, Puesto: Puesto, Usuario: Usuario })
+    navigate("/proyectos")
+  }
+  useEffect(() => {
+    const getCharactersById = async () => {
+
+      const response = await axios.get(`${url}/${id}`)
+      setEstado(response.data.titulo)
+      setOficina(response.data.imagen)
+      setPuesto(response.data.git)
+      setUsuario(response.data.descripcion)
+    }
+
+    getCharactersById()
+  }, [id])
+
   return (
-    <div>EditarUsuario</div>
+    <>
+      {<div className="contenedor-con-fondo">
+        <img className='imagenfondo' src={fondo} />
+      </div>}
+      <div className="contenedor-form"><h2>Editar Repositorio</h2>
+        <div className="contenedor-form1">
+          <div className="formulario">
+            <form onSubmit={update}>
+              <div>
+                <label>Titulo:</label>
+                <input type="text" value={Estado} onChange={(e) => setEstado(e.target.value)} />
+              </div>
+              <div>
+                <label>Imagen:</label>
+                <input type="text" value={Oficina} onChange={(e) => setOficina(e.target.value)} />
+              </div>
+              <div>
+                <label>Git:</label>
+                <input type="text" value={Puesto} onChange={(e) => setPuesto(e.target.value)} />
+              </div>
+              <div>
+                <label>Descripción:</label>
+                <textarea name="" id="" cols="30" rows="10" value={Usuario} onChange={(e) => setUsuario(e.target.value)}>
+                </textarea>
+              </div>
+              <button type="submit"><Icon icon="bxs:edit" color="blue" /></button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
