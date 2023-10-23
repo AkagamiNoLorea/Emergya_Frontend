001464c9fdf011dc347929d60fdc276d5e17a67a
xavi <xavilok@gmail.com>
2023-10-23 15:00:52 +0200

    se a creado crearreserva

--- src/components/reservas/CrearReserva.jsx
+++ src/components/reservas/CrearReserva.jsx
@@ -1,12 +1,13 @@
-import axios from "axios"
+import '../estilos/Crearreserva.css'
+/* import axios from "axios"
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
-const url = "http://localhost:8086/api/v1/usuario"
+const url = "http://localhost:8086/api/v1/usuario"  */
 
 const CrearUsuario = () => {
 
-    const [Nombreoficina, setNombreoficina,] = useState('')
+    /* const [Nombreoficina, setNombreoficina,] = useState('')
     const [Puesto, setPuesto] = useState('')
     const [Planodistribucion, setPlanodistribucion] = useState('')
     const [Diasantelacion, setDiasantelacion] = useState('')
@@ -17,7 +18,7 @@ const CrearUsuario = () => {
         e.preventDefault()
         await axios.post(url, { Nombreoficina: Nombreoficina, Puesto: Puesto, Planodistribucion: Planodistribucion, Diasantelacion: Diasantelacion })
         navigate("/proyectos")
-    }
+    }  */
 
     return (
         <>
@@ -26,28 +27,74 @@ const CrearUsuario = () => {
             <h2>Puestos estado</h2>
             <div className="contenedor-form">
                 <div className="contenedor-form1">
-                    <div className="formulario">
-                        <form onSubmit={guardar}>
-                            <div>
-                                <label>Titulo:</label>
-                                <input type="text" value={Nombreoficina} onChange={(e) => setNombreoficina(e.target.value)} />
-                            </div>
-                            <div>
-                                <label>Imagen:</label>
-                                <input type="text" value={Puesto} onChange={(e) => setPuesto(e.target.value)} />
-                            </div>
-                            <div>
-                                <label>Git:</label>
-                                <input type="text" value={Planodistribucion} onChange={(e) => setPlanodistribucion(e.target.value)} />
-                            </div>
-                            <div>
-                                <label>Descripción:</label>
-                                <textarea name="" id="" cols="30" rows="10" value={Diasantelacion} onChange={(e) => setDiasantelacion(e.target.value)}>
-                                </textarea>
-                            </div>
-                            <button></button>
-                        </form>
+                    
+                    <div className='contenedor-puesto'>
+                        <button>1</button>
+                        <button>2</button>
+                        <button>3</button>
+                        <button>4</button>
+                        <button>5</button>
+                        <button>6</button>
+                        <button>7</button>
+                        <button>8</button>
+                        <button>9</button>
+                        <button>10</button>
+                        <button>11</button>
+                        <button>12</button>
+                        <button>13</button>
+                        <button>14</button>
+                        <button>15</button>
+                        <button>16</button>
+                        <button>17</button>
+                        <button>18</button>
+                        <button>19</button>
+                        <button>20</button>
+                        <button>21</button>
+                        <button>22</button>
+                        <button>23</button>
+                        <button>24</button>
+                        <button>25</button>
+                        <button>26</button>
+                        <button>27</button>
+                        <button>28</button>
+                        <button>29</button>
+                        <button>30</button>
+                        <button>31</button>
+                        <button>32</button>
+                        <button>33</button>
+                        <button>34</button>
+                        <button>35</button>
+                        <button>36</button>
+                        <button>37</button>
+                        <button>38</button>
+                        <button>39</button>
+                        <button>40</button>
+                        <button>41</button>
+                        <button>42</button>
+                        <button>43</button>
+                        <button>44</button>
+                        <button>45</button>
+                        <button>46</button>
+                        <button>47</button>
+                        <button>48</button>
+                        <button>49</button>
+                        <button>50</button>
+                        <button>51</button>
+                        <button>52</button>
                     </div>
+                    <div className="leyendaBox">
+                        <ul>
+                            <li><div className="color"></div><h3>No Disponible</h3></li>
+                            <li><div className="color"></div><h3>Tu reserva</h3></li>
+                            <li><div className="color"></div><h3>Libre</h3></li>
+                            <li><div className="color"></div><h3>Ocupado mañana</h3></li>
+                            <li><div className="color"></div><h3>Ocupado tarde</h3></li>
+                            <li><div className="color"></div><h3>Ocupado día</h3></li>
+
+                        </ul>
+
+                    </div>
+
                 </div>
             </div>
         </>
