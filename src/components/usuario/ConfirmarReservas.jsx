a7fa39f3db28d7fd9ed21d414853283bd8f3e176
encarnasantanahormiga <santanahormigaencarna@gmail.com>
2023-10-24 12:02:29 +0100

    creación de cars

---
+++ src/components/usuario/ConfirmarReservas.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const ConfirmarReservas = () => {
+    return (
+        <>
+
+            <div className='container'>
+                <h1>Fecha</h1>
+                <br></br>
+                <h2>Dias en número</h2>
+                <div>
+                    <h1>Reservado:</h1>
+                    <p></p>
+                    <h2>Todo el día</h2>
+                    <botton className='bt1'>Nombre de la oficina</botton>
+                    <div>
+                        <h1>Puesto reservado:</h1>
+                        <p></p>
+                        <h2>25</h2>
+                        <p></p>
+                        <botton className='bt2'>Cancelar reserva</botton>
+                    </div>
+                </div>
+            </div>
+
+
+        </>
+    )
+}
+
+export default ConfirmarReservas
